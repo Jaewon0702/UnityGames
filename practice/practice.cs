33dfc3e57851f251f038d13f8001933be4bf0e8b
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-01-29 17:46:40 +0900

    Update practice.cs

--- practice/practice.cs
+++ practice/practice.cs
@@ -21,6 +21,26 @@ public class practice : MonoBehaviour
             0);
         transform.Translate(vec); //Translate: 벡터 값을 현재 위치에 더하는 함수
         //벡터의 크기만큼 이동
+        
+        //1. MoveTowards
+
+       /* transform.position =
+            Vector3.MoveTowards(transform.position, target, 2f); //(현재 위치, 목표 위치, 속도) 
+
+        //2. SmoothDamp: 부드러운 감속 이동, 마지막 메개변수에 반비례하여 속도 증가
+
+        Vector3 velo = Vector3.zero;
+
+        transform.position = 
+            Vector3.SmoothDamp(transform.position, target, ref velo, 1.0f);
+
+        //3. Lerp
+        transform.position = 
+            Vector3.Lerp(transform.position, target, 0.05f);
+
+        //4. SLerp(구면 선형 보간)
+        transform.position = 
+        Vector3.Slerp(transform.position, target, 0.05f);
     }
 
     // Update is called once per frame
