6a4e509f07335f915d23fd0a4b158d9e0598ab54
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-02-04 21:18:37 +0900

    Move, Collision, Collider

---
+++ practice/Collision1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    MeshRenderer mesh;
+    Material mat;
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) { //물리적 충돌이 시작될 때 호출되는 함수
+
+        if(collision.gameObject.name == "Sphere")
+            mat.color = new Color(0, 0, 0);
+    }
+
+    private void OnCollisionExit(Collision collision) { //물리적 충돌이 끝났을때 호출되는 함수
+
+        if(collision.gameObject.name == "Sphere")
+            mat.color = new Color(1, 1, 1);
+    }
+
+    /*
+     private void OnCollisionStay(Collision other) { //물리적 충돌 중일때 호출되는 함수
+        
+    }
+
+    private void OnCollisionExit(Collision other) { //물리적 충돌이 끝났을 때 호출
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ practice/Move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() //Rigid 관련 코드는 FixedUpdate안에 작성!
+    {   //#1. 속력 바꾸기
+        //rigid.velocity = new Vector3(2, 4, 3); 
+
+        //#2. 힘을 가하기
+        
+        if(Input.GetButtonDown("Jump")){
+        rigid.AddForce(Vector3.up * 100, ForceMode.Impulse); //주로 캐릭터가 점프할 때 사용!!
+        Debug.Log(rigid.velocity);
+        }
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        rigid.AddForce(vec, ForceMode.Impulse);
+        
+
+        //#3 회전력
+        //rigid.AddTorque(Vector3.up); //AddTorgque(Vec) Vec 방향을 축으로 회전력이 생김.
+        
+    }
+
+    private void OnTriggerStay(Collider other) { //OnTrigger는 물리적인 충돌이 아닌 둘이 겹쳐있는 가를 보기 때문에 Collider 이용!
+        if(other.name == "Cube")
+            rigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+    }
+
+    
+}
