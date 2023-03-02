3b33eccf8d87523e593c4cc2eb79c8987b5fc1bd
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-03-02 20:09:12 +0900

    Add Falling Algorithm

--- Rolling/GameManagerLogic.cs
+++ Rolling/GameManagerLogic.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerLogic : MonoBehaviour{
     public int totalItemCount;
     public int stage; // 매 Scene마다 숫자를 바꿔야함!!
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player"){
+            SceneManager.LoadScene(stage); //각 장면의 index 사용 가능!
+        }
+    }
 }
