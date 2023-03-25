e529dd4a9a55c315ed092a58aa1811a0d8e2ca29
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-03-25 20:25:05 +0900

    Direction Sprite, Animation, Jump

--- PlatFormer/Scripts/PlayerMove.cs
+++ PlatFormer/Scripts/PlayerMove.cs
@@ -6,18 +6,43 @@ using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        //Jump
+         if(Input.GetButtonDown("Jump")){
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+         }
         //Stop Speed
         if(Input.GetButtonUp("Horizontal")){
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.3f, rigid.velocity.y);
         }
+
+        //Direction Sprite
+        if(Input.GetButtonDown("Horizontal")){
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        
+        //Animation
+        if(Mathf.Abs(rigid.velocity.x) < 0.5){
+            anim.SetBool("isWalking", false);
+        }
+        else{
+            anim.SetBool("isWalking", true);
+        }
+
         
     }
 
@@ -27,6 +52,7 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
+        //Max Speed
         if(rigid.velocity.x > maxSpeed){ //Right Max Speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
