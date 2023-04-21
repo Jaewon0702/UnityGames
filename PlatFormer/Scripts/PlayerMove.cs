35b1a386cb4b73472ec831b1f50a5ac7cb781b7e
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-04-21 19:45:53 +0900

    Add Landing Platform & Block Jump several times

--- PlatFormer/Scripts/PlayerMove.cs
+++ PlatFormer/Scripts/PlayerMove.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float maxSpeed;
-    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
+    public float maxSpeed;
+    public float jumpPower;
+
+    // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -20,13 +21,14 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         //Jump
-         if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")){
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
 
-         }
+        }
         //Stop Speed
         if(Input.GetButtonUp("Horizontal")){
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.3f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 1.0f, rigid.velocity.y);
         }
 
         //Direction Sprite
@@ -34,7 +36,6 @@ public class PlayerMove : MonoBehaviour
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
 
-        
         //Animation
         if(Mathf.Abs(rigid.velocity.x) < 0.5){
             anim.SetBool("isWalking", false);
@@ -43,24 +44,32 @@ public class PlayerMove : MonoBehaviour
             anim.SetBool("isWalking", true);
         }
 
-        
     }
-
-    void FixedUpdate(){
-
-        //Move by key control
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move by Key Control
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
         //Max Speed
-        if(rigid.velocity.x > maxSpeed){ //Right Max Speed
+        if(rigid.velocity.x > maxSpeed){//Right Max Speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
         else if(rigid.velocity.x < maxSpeed * (-1)){ //Left Max Speed
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
         }
-        
-    }
-    // Start is called before the first frame update
 
+        //Landing Platform
+        if(rigid.velocity.y < 0){
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if(rayHit.collider != null){
+            if(rayHit.distance < 1.0f){
+                anim.SetBool("isJumping", false);
+            }
+        }
+        }
+    }
 }
