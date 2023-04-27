a02658246619d923098c690dbee4a3b104c495e8
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-04-27 21:18:16 +0900

    Health down

--- PlatFormer/Scripts/GameManager.cs
+++ PlatFormer/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public int stagePoint;
     public int stageIndex;
     public int health;
+    public PlayerMove player;
     // Start is called before the first frame update
     public void NextStage()
     {
@@ -17,15 +18,37 @@ public class GameManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    public void HealthDown(){
+        if(health > 1){
+            health--;
+        }
+        else{
+            //Player Death Effect
+            player.OnDie();
+            health = 0;
+            //Result UI
+            Debug.Log("플레이어가 죽었습니다!");
+            //Retry Button UI
+
+        }
+    }
+
+    // When Falling down
      void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.tag == "Player"){
-            //Health down
-            health--;
+            
 
             //Player Reposition
-            collision.attachedRigidbody.velocity = Vector2.zero; //Velocity Reset
-            collision.transform.position = new Vector3(-6, 0.21f, 0);
+            if(health > 1){
+                collision.attachedRigidbody.velocity = Vector2.zero; //Velocity Reset
+                collision.transform.position = new Vector3(-6, 0.21f, 0);
+                HealthDown();
+            }
+            else{
+            //Health down
+                health = 0;
+                HealthDown();
+            }
 
         }
         }
