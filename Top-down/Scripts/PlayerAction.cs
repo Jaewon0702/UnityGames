614049c881fab8a7682a3b003f3cfbfe5cb625b1
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-05-10 20:32:25 +0900

    Add Scan Action

--- Top-down/Scripts/PlayerAction.cs
+++ Top-down/Scripts/PlayerAction.cs
@@ -9,9 +9,12 @@ public class PlayerAction : MonoBehaviour
     float h;
     float v;
     bool isHorizonMove;
+    Vector3 dirVec;
 
     Rigidbody2D rigid;
     Animator anim;
+
+    GameObject scanObject;
 
    void Awake(){
     rigid = GetComponent<Rigidbody2D>();
@@ -56,6 +59,50 @@ public class PlayerAction : MonoBehaviour
         else{
             anim.SetBool("isChange", false);
         }
+
+        //Direction
+        if(vDown && v == 1){
+            dirVec = Vector3.up;
+        }
+        else if(vDown && v == -1){
+            dirVec = Vector3.down;
+        }
+        else if(hDown && h == -1){
+            dirVec = Vector3.left;
+        }
+        else if(hDown && h == 1){
+            dirVec = Vector3.right;
+        }
+
+        //Scan Object
+        if(Input.GetButtonDown("Jump") && scanObject != null){
+            Debug.Log("This is : " + scanObject.name);
+        }
+        
+        
+
+    }
+
+    void FixedUpdate(){
+        //Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;
+
+        //Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null){
+            scanObject = rayHit.collider.gameObject;
+        }
+        else{
+            scanObject = null;
+        }
+        
+
+    }
+}
+
         
         
 
