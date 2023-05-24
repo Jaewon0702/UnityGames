65dabe6383d30f9f8b37b32545f6cc9877218cbe
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-05-24 20:46:26 +0900

    Added Next Quest & Control Object Logic.

--- Top-down/Scripts/QuestManager.cs
+++ Top-down/Scripts/QuestManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     public int questId;
-    public int questActionIndex;
+    public int questActionIndex; //Order of Quest Talk
+    public GameObject[] questObject;
+
     Dictionary<int, QuestData> questList;
 
     void Awake()
@@ -17,19 +19,54 @@ public class QuestManager : MonoBehaviour
 
     void GenerateData()
     {
-        questList.Add(10, new QuestData("Talking to Villagers", new int[]{1000, 2000}));
+        questList.Add(10, new QuestData("Talking to Villagers", new int[]{1000, 2000})); //new int[]안에는 quest와 연관된 Npc의 Id가 입력되어 있음.
+        questList.Add(20, new QuestData("Finding Ludo's Coin", new int[]{5000, 2000}));
+        questList.Add(30, new QuestData("Quest All Clear!", new int[]{0}));
+
     }
 
     public int GetQuestTalkIndex(int id){
         return questId + questActionIndex;
     }
 
-    public void CheckQuest(int id){
-
-        if(id == questList[questId].npcId[questActionIndex]){
+    public string CheckQuest(int id){
+    
+        //Next Quest Target
+        if(id == questList[questId].npcId[questActionIndex]){ //QuestData 함수의 npcId 배열(new int[]{1000, 2000}))의 Index 
+        //Ex) questActionIndex == 0이면 0번째인 1000반환
+            //순서에 맞게 대화했을 때만 QuestActionIndex++;
             questActionIndex++;
+        }
+
+        //Control Quest Object
+         ControlObject();
+
+        if(questActionIndex == questList[questId].npcId.Length){
+            NextQuest();
+        }
+        //Quest Name
+        return questList[questId].questName;
+    }
+    void NextQuest(){
+        questId += 10;
+        questActionIndex = 0;
+    }
+
+    void ControlObject(){
+        switch(questId){
+            case 10:
+                if(questActionIndex == 2){
+                    questObject[0].SetActive(true);
+                }
+            break;
+            case 20:
+                if(questActionIndex == 1){
+                    questObject[0].SetActive(false);
+                }
+            break;
         }
     }
 
 
 }
+
