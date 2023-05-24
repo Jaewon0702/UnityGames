3b463c45e6ae6b8560dcf5e6de4e1d2145183a7a
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-05-24 20:48:51 +0900

    Added New Talk Data following new quest

--- Top-down/Scripts/TalkManager.cs
+++ Top-down/Scripts/TalkManager.cs
@@ -28,10 +28,19 @@ public class TalkManager : MonoBehaviour
                                          "There's an amazing legend in this town.:1",
                                          "If you go to the lake on the right, Ludo will tell you.:1"});
 
-    talkData.Add(10 + 2000 + 1, new string[]{"Hey!:0 ",
+    talkData.Add(10 + 1 + 2000, new string[]{"Hey!:0 ",
                                          "Are you here to hear the legend of this lake?:1",
                                          "Then I'd like you to do work.:1",
-                                         "I want you to pick up a coin that fell near my house.:1"});
+                                         "I want you to pick up a coin that fell near my house.:1"}); //questId + NpcId + questActionIndex;
+
+   talkData.Add(20 + 1000, new string[]{"Ludo's Coin?:1",
+                                         "Don't drop the money!:3",
+                                         "I'll have to say something to Ludo later.:3"}); 
+   talkData.Add(20 + 2000, new string[]{"If you find it, make sure to bring it.:1"}); 
+   talkData.Add(20 + 5000, new string[]{"I found a coin."});
+   talkData.Add(20 + 1 + 2000, new string[]{"Thank you for finding.:2"});
+   
+   
 //Portrait Data
 //+0: Idle, +1:Talk, +2: Smile, +3: Angry
     portraitData.Add(1000 + 0, portraitArr[0]); 
@@ -39,7 +48,7 @@ public class TalkManager : MonoBehaviour
     portraitData.Add(1000 + 2, portraitArr[2]); 
     portraitData.Add(1000 + 3, portraitArr[3]);
 
-    portraitData.Add(2000 + 0, portraitArr[4]); //Idle
+    portraitData.Add(2000 + 0, portraitArr[4]); //Idle 
     portraitData.Add(2000 + 1, portraitArr[5]); //Talk
     portraitData.Add(2000 + 2, portraitArr[6]); //Smile
     portraitData.Add(2000 + 3, portraitArr[7]); //Angry
@@ -63,5 +72,6 @@ public class TalkManager : MonoBehaviour
    }
 
 }
+
 
 
