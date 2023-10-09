a4fde41043eb2b4da7ec00c80db8bf1b1675d7ff
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-10-09 18:54:32 +0900

    Added bool to Judge the Camera is broken or not
    To Fix the camera position broken pieces

--- Rolling/CameraMove.cs
+++ Rolling/CameraMove.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CameraMove : MonoBehaviour
-{
-    Transform playerTransform;
-    Vector3 Offset;
-    public Fracture fracture;
+public class Fracture : MonoBehaviour
+{   public GameObject pieces; 
+    public GameObject smartphone;
+    public GameObject mainCamera;   
+    public bool broken;
 
     void Awake(){
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        Offset = transform.position - playerTransform.position;
+        broken = false; // Is Camera Broken?
     }
-    void LateUpdate()
-    {
-        if(fracture.broken)
-        // When the phone breaks, the camera is reflected in the piece
-        transform.position = GameObject.FindGameObjectWithTag("Piece").transform.position + Offset; 
-        else
-        transform.position = playerTransform.position + Offset;
+    // Start is called before the first frame update
+
+    public void OnFracture(){
+        smartphone.GetComponent<Renderer>().enabled = false; //enabled and SetActive has Same Effct
+        pieces.SetActive(true);
+        transform.position =  GameObject.FindGameObjectWithTag("Player").transform.position; //Same Position with Player
+        transform.localRotation = GameObject.FindGameObjectWithTag("Player").transform.localRotation;//Same Rotation with Player 
+        broken = true;
+        //5초 정도 있다가 다시 시작하는 로직을 추가하자.
     }
 }
+
