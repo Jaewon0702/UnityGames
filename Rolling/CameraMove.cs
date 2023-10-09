306213dc6a25b473f594dc0daa9cc3087493450c
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-10-09 18:52:05 +0900

    Fix the camara position
    When the phone breaks, the camera is reflected in the piece

--- Rolling/CameraMove.cs
+++ Rolling/CameraMove.cs
@@ -6,16 +6,18 @@ public class CameraMove : MonoBehaviour
 {
     Transform playerTransform;
     Vector3 Offset;
+    public Fracture fracture;
 
     void Awake(){
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         Offset = transform.position - playerTransform.position;
     }
- 
-
-
     void LateUpdate()
     {
+        if(fracture.broken)
+        // When the phone breaks, the camera is reflected in the piece
+        transform.position = GameObject.FindGameObjectWithTag("Piece").transform.position + Offset; 
+        else
         transform.position = playerTransform.position + Offset;
     }
 }
