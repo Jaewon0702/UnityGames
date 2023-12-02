5d8e5c7c9711d53f951d6637c385b3ef71485a33
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2023-12-02 17:03:46 +0900

    Camera rotates around the phone.
    Made the Camera rotates by drawing a sphere around the phone.
    (Shifted the center point of the sphere from the camera to the phone.)
    Previously, it was rotated around the camera itself.

--- Rolling/Scripts/CameraMove.cs
+++ Rolling/Scripts/CameraMove.cs
@@ -41,41 +41,53 @@ public class CameraMove : MonoBehaviour
         dirNormalized = realCamera.localPosition.normalized;
         finalDistance = realCamera.localPosition.magnitude; // size
 
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    void Update() {
+    /*void Update() {
+        
+        
+    }*/
+    void LateUpdate() {
         //1. Get Mouse Button
-        rotX += Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime * -1f;
-        rotY += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        //Input.GetMouseButtonDown(0)
+        float x = Input.GetAxis("Mouse Y");
+        float y = Input.GetAxis("Mouse X");
+        // For Use in Moblie
+        if(Input.touchCount > 0){
+            x = Input.touches[0].deltaPosition.y;
+            y = Input.touches[0].deltaPosition.x;
+        }
+        rotX += x * sensivity * Time.deltaTime * -1f;
+        rotY += y * sensivity * Time.deltaTime;
+        
         //2. Limit the angle to prevent the camera from turning.
         rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
         //3. Rotate Camera
         Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
         transform.rotation = rot;
-        
-    }
-    void LateUpdate() {
-        Reflect();   
 
         transform.position = Vector3.MoveTowards(transform.position, objectToFollow.position, followSpeed * Time.deltaTime);
-        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+        // Zoom in if there is an object between the camera and the player
+        /*finalDir = transform.TransformPoint(dirNormalized * maxDistance);
 
         if(Physics.Linecast(transform.position, finalDir, out rayHit))
             finalDistance = Mathf.Clamp(rayHit.distance, minDistance, maxDistance);
         else
-            finalDistance = maxDistance;
+            finalDistance = maxDistance;*/
 
-        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormalized * finalDistance, Time.deltaTime * smoothness);
+        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormalized * maxDistance, Time.deltaTime * smoothness);
+        Reflect();
 
 
     }
     void Reflect(){
         if(fracture.broken) // When the phone breaks, the camera is reflected in the piece
         transform.position = GameObject.FindGameObjectWithTag("Piece").transform.position + Offset; 
-        else transform.position = playerTransform.position + Offset;
+        //else transform.position = playerTransform.position + Offset;
+
     }
 }
+
 
 
