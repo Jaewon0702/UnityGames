b83e72c67b69dc006d3a0099ae3b67ac74b001f2
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2024-01-15 18:26:42 +0900

    Use SetActive instead of Renderer.
    Bug: Non-broken smartphone remains with pieces when heath is 0.
    So Used SetActive.

--- Rolling/Scripts/Fracture.cs
+++ Rolling/Scripts/Fracture.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fracture : MonoBehaviour
 {   public GameObject pieces; 
-    public GameObject smartphone;   
+    public GameObject smartphone;
+    public GameObject mainCamera;   
+    public bool broken;
+
+    void Awake(){
+        broken = false; // Is Camera Broken?
+    }
     // Start is called before the first frame update
 
     public void OnFracture(){
-        smartphone.GetComponent<Renderer>().enabled = false; //enabled and SetActive has Same Effct
-        pieces.SetActive(true);
         transform.position =  GameObject.FindGameObjectWithTag("Player").transform.position; //Same Position with Player
         transform.localRotation = GameObject.FindGameObjectWithTag("Player").transform.localRotation;//Same Rotation with Player 
+        pieces.SetActive(true);
+        //smartphone.GetComponent<Renderer>().enabled = false; //enabled and SetActive has Same Effct
+        smartphone.SetActive(false);
+        broken = true;
+        //5초 정도 있다가 다시 시작하는 로직을 추가하자.
     }
 }
-
